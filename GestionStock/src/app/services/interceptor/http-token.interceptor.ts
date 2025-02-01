a4291673c93dcea6bd9fe9520dca3db531d17593
Pierre Maurand Ovassa <pierremaurand@gmail.com>
2025-02-01 23:08:24 +0100

    update app

---
+++ GestionStock/src/app/services/interceptor/http-token.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from '../token/token.service';
+
+export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService);
+  const token = tokenService.token;
+
+  if(token) {
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next(authReq);
+  }
+  return next(req);
+};
